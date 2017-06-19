32c6b8e8d5384cdd4d1021aacdb8ac1e5629e2fb
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-06-19 16:37:22 +0200

    Add ActiveModel::Model for rails versions < 4.

--- lib/rails-add_ons.rb
+++ lib/rails-add_ons.rb
@@ -4,5 +4,6 @@ require 'twitter_bootstrap_components_rails'
 require 'simple_form'
 require 'responders' if Rails::VERSION::MAJOR > 3
 require 'rails-i18n'
+require 'active_model/model' if Rails::VERSION::MAJOR < 4
 
 require 'rails/add_ons'
