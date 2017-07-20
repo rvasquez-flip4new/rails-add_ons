095f1951c18babdbd04132aaf63391032a353fe3
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-07-20 15:48:35 +0200

    Refactor exception handling into its own method.

--- app/concerns/api_controller_concerns/exception_handling.rb
+++ app/concerns/api_controller_concerns/exception_handling.rb
@@ -4,19 +4,25 @@ module ApiControllerConcerns
 
     included do
       rescue_from Exception do |exception|
-        if Rails.env.development? || Rails.env.test?
-          error = { message: exception.message }
+        handle_exception(exception)
+      end
+    end
 
-          error[:application_trace] = Rails.backtrace_cleaner.clean(exception.backtrace)
-          error[:full_trace] = exception.backtrace 
+    private
 
-          respond_to do |format|
-            format.json { render json: error, status: 500 }
-          end
-        else
-          respond_to do |format|
-            format.json { render json: { error: 'Internal server error.' }, status: 500 }
-          end
+    def handle_exception(exception)
+      if Rails.env.development? || Rails.env.test?
+        error = { message: exception.message }
+
+        error[:application_trace] = Rails.backtrace_cleaner.clean(exception.backtrace)
+        error[:full_trace] = exception.backtrace
+
+        respond_to do |format|
+          format.json { render json: error, status: 500 }
+        end
+      else
+        respond_to do |format|
+          format.json { render json: { error: 'Internal server error.' }, status: 500 }
         end
       end
     end
